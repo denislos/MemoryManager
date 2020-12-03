234e88351e8d11f90d49bc673ae1f3e04ee2c3ab
Petrenko Aleksey <androror@gmail.com>
2020-12-03 23:13:38 +0300

    Зафиксировали интерфейс менеджера

--- memman/memman.h
+++ memman/memman.h
@@ -6,15 +6,6 @@
 // global user callback
 extern void (*_mm_callback_)(void* addr, int code);
 
-// in case of error we call user callback
-void mm_attach_callback(void (*callback)(void* addr, int code));
-void mm_init();
-
-
-#define DEREF(ptr, type) (check_type(ptr, type) ? (type*)ptr : _mm_callback_(0, 0))
-
-#define ALLOC(type)
-
 // converts a type name WITHOUT qualifiers to constant name
 #define MM_TYPEIDNAME(type) __MM_TYPEID_##type##__
 #define MM_UNITNAME(type) __MM_UNIT_##type##__
@@ -26,6 +17,19 @@ void mm_init();
 #define MM_BLOCKSIZE MM_BLOCKSIZE_DEFAULT
 #endif
 
+//***********************************
+//USER INTERFACE
+//***********************************
+void mm_attach_callback(void (*callback)(void* addr, int code));
+void mm_init();
+#define MM_DEREF(ptr, type) (check_type(ptr, type) ? (type*)ptr : _mm_callback_(0, 0))
+#define MM_ALLOC(type) 0 //returns (type*)
+#define MM_DEALLOC(ptr)
+#define MM_COMPARE(ptr1, ptr2) true
+#define MM_NULLPTR 0
+#define MM_VERIFY_PTR(ptr, type) true
+#define MM_VERIFY_EMPTY true
+
 // defines an integer constant for type identification, unique for each call
 #define MM_TYPE_REGISTER(type) enum{ MM_TYPEIDNAME(type)=__COUNTER__}; \
 typedef struct \
@@ -34,16 +38,12 @@ typedef struct \
 	int type_id;\
 	void* hdr;\
 	type data;\
-} MM_UNITNAME(type);\
-typedef struct \
-{\
-	void* prev;\
-	void* next;\
-	int used_cnt;\
-	MM_UNITNAME(type) data[MM_BLOCKSIZE];\
-}MM_BLOCKNAME(type);
+} MM_UNITNAME(type);
 
-#define MM_END_TYPE_REGISTER
+
+//***********************************
+//END USER INTERFACE
+//***********************************
 
 
 
