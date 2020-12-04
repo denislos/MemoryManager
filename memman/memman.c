4c6a98c55ad72c13be3e38d506db1b5ad29226ed
Petrenko Aleksey <androror@gmail.com>
2020-12-04 21:14:54 +0300

    baseline готов

--- memman/memman.c
+++ memman/memman.c
@@ -4,16 +4,22 @@
 extern "C" {
 #endif
 
-void (*_mm_callback_)(void* addr, int code);
+static void (*_mm_callback_)(void* addr, int code);
+static unsigned int _mm_global_alloc_;
 
 void mm_attach_callback(void (*callback)(void* addr, int code))
 {
 	_mm_callback_ = callback;
 }
 
+static void _mm_default_callback_(void* addr, int code)
+{
+	exit(code);
+}
+
 void mm_init()
 {
-	_mm_callback_ = 0;
+	_mm_callback_ =_mm_default_callback_;
 }
 
 void* _mm_alloc_(size_t size, int type_id)
@@ -21,15 +27,79 @@ void* _mm_alloc_(size_t size, int type_id)
 	MM_UNITNAME(MM_TEMPLATE)* unit = (MM_UNITNAME(MM_TEMPLATE) * )malloc(size);
 	if (!unit) return 0;
 	unit->busy = 1;
+	unit->size = size;
+	_mm_global_alloc_ += size;
 	unit->type_id = type_id;
 	return (void*)unit;
 }
 
 void _mm_free_(void* ptr)
 {
-	MM_UNITNAME(MM_TEMPLATE)* unit = MM_UNIT_HDR(ptr);
+	MM_UNITNAME(MM_TEMPLATE)* unit;
+
+	if (ptr)
+	{
+		unit = MM_UNIT_HDR(ptr);
+	}
+	else
+	{
+		_mm_callback_(ptr, EMPTY_PTR_OP);
+		return;
+	}
+
+	if(!unit->busy)
+	{
+		_mm_callback_(ptr, EMPTY_PTR_OP);
+		return;
+	}
+
 	unit->busy = 0;
+	_mm_global_alloc_ += unit->size;
 	free((void*)unit);
+}
+
+int _mm_compare_(void* ptr1, void* ptr2)
+{
+	MM_UNITNAME(MM_TEMPLATE) *unit1, *unit2;
+	if (ptr1 && ptr2)
+	{
+		unit1 = MM_UNIT_HDR(ptr1);
+		unit2 = MM_UNIT_HDR(ptr2);
+		if (unit1->type_id != unit2->type_id)
+		{
+			_mm_callback_(ptr1, TYPE_DISREP);
+			return 0;
+		}
+		if (!unit1->busy)
+		{
+			_mm_callback_(ptr1, EMPTY_PTR_OP);
+			return 0;
+		}
+		if (!unit2->busy)
+		{
+			_mm_callback_(ptr2, EMPTY_PTR_OP);
+			return 0;
+		}
+		return ptr1 == ptr2;
+	}
+	return !(ptr1 || ptr2);
+}
+
+void _mm_verify_(void* ptr, int type_id)
+{
+	MM_UNITNAME(MM_TEMPLATE)* unit;
+	if (!ptr) return;
+	unit = MM_UNIT_HDR(ptr);
+	if (!unit->busy) 
+		_mm_callback_(ptr, EMPTY_PTR_OP);
+	if (unit->type_id != type_id)
+		_mm_callback_(ptr, TYPE_DISREP);
+	return;
+}
+
+void _mm_verify_empty_()
+{
+	if(_mm_global_alloc_) _mm_callback_(0, ALLOCK_NOT_EMPTY);
 }
 
 #ifdef __cplusplus
--- memman/memman.h
+++ memman/memman.h
@@ -5,7 +5,8 @@
 #include <stdint.h>
 
 // global user callback
-extern void (*_mm_callback_)(void* addr, int code);
+//extern void (*_mm_callback_)(void* addr, int code);
+//extern unsigned int _mm_global_alloc_;
 
 // converts a type name WITHOUT qualifiers to constant name
 #define MM_TYPEIDNAME(type) __MM_TYPEID_##type##__
@@ -20,6 +21,14 @@ extern void (*_mm_callback_)(void* addr, int code);
 
 #define MM_NULLPTR 0
 
+enum
+{
+	OK = 0,
+	TYPE_DISREP,
+	EMPTY_PTR_OP,
+	ALLOCK_NOT_EMPTY,
+};
+
 typedef struct
 {
 	size_t cur_offset;
@@ -31,6 +40,7 @@ typedef struct
 	int busy;\
 	int type_id;\
 	__mm_block__* hdr;\
+	size_t size;\
 	type data;\
 } MM_UNITNAME(type);
 
@@ -39,21 +49,33 @@ MM_UNITSTR(MM_TEMPLATE);
 
 #define MM_UNIT_HDR(ptr)  ((MM_UNITNAME(MM_TEMPLATE)*)((char*)ptr - offsetof(MM_UNITNAME(MM_TEMPLATE), data)))
 #define MM_DATA_PTR(strptr, type) (type*)(strptr ? &(((MM_UNITNAME(type)*)strptr)->data) : MM_NULLPTR)
- 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void* _mm_alloc_(size_t size, int type_id);
 void _mm_free_(void* ptr);
+int _mm_compare_(void* ptr1, void* ptr2);
+void _mm_verify_(void* ptr, int type_id);
+void _mm_verify_empty_();
 
 //***********************************
 //USER INTERFACE
 //***********************************
 void mm_attach_callback(void (*callback)(void* addr, int code));
 void mm_init();
+
+#ifdef __cplusplus
+}
+#endif
+
 #define MM_DEREF(ptr, type) (check_type(ptr, type) ? (type*)ptr : _mm_callback_(0, 0))
 #define MM_ALLOC(type) MM_DATA_PTR(_mm_alloc_(sizeof(MM_UNITNAME(type)), type)
 #define MM_DEALLOC(ptr) _mm_free_((void*)ptr)
-#define MM_COMPARE(ptr1, ptr2) true
-#define MM_VERIFY_PTR(ptr, type) true
-#define MM_VERIFY_EMPTY true
+#define MM_COMPARE(ptr1, ptr2) _mm_compare_(ptr1, ptr2)
+#define MM_VERIFY_PTR(ptr, type) _mm_verify_(ptr, MM_TYPEIDNAME(type))
+#define MM_VERIFY_EMPTY _mm_verify_empty_()
 
 // defines an integer constant for type identification, unique for each call
 #define MM_TYPE_REGISTER(type) enum{ MM_TYPEIDNAME(type)=__COUNTER__}; \
