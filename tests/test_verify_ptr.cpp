bbc3bcd2babe6dc80013ef7fd672f0b322fd45e3
Danil Yarovoy <memorex900@gmail.com>
2020-12-08 02:43:48 +0300

    added verify pointer tests

--- tests/test_verify_ptr.cpp
+++ tests/test_verify_ptr.cpp
@@ -4,20 +4,76 @@
 
 #include "memman.h"
 
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    int i;
+    char c;
+} custom_t;
+
+void dummy_callback([[maybe_unused]] void *addr, [[maybe_unused]] int code) {
+
+}
+
 MM_TYPE_REGISTER(int);
 MM_TYPE_REGISTER(char);
+MM_TYPE_REGISTER(custom_t);
 
-TEST(TestVerifyPtr, TestTentative)
+TEST(TestVerifyPtr, TestNull)
 {
-    [[maybe_unused]] int *first = MM_ALLOC(int);
-    [[maybe_unused]] char *second = MM_ALLOC(char);
-    //ASSERT_TRUE(MM_VERIFY_PTR(first, int));
-    //ASSERT_TRUE(MM_VERIFY_PTR(second, char));
-    // TODO uncomment when done
-    // EXPECT_FALSE(MM_VERIFY_PTR(second, int));
-    // EXPECT_FALSE(MM_VERIFY_PTR(first, char));
-    // EXPECT_FALSE(MM_VERIFY_PTR(first, wrong_name));
+    mm_init();
+    mm_attach_callback(dummy_callback);
 
-    MM_DEALLOC(first);
-    MM_DEALLOC(second);
+    EXPECT_FALSE(MM_VERIFY_PTR(NULL, int));
+    EXPECT_FALSE(MM_VERIFY_PTR(NULL, char));
+    EXPECT_FALSE(MM_VERIFY_PTR(NULL, custom_t));
 }
+
+TEST(TestVerifyPtr, TestTypeMatching)
+{
+    mm_init();
+    mm_attach_callback(dummy_callback);
+
+    int *iptr = MM_ALLOC(int);
+    char *cptr = MM_ALLOC(char);
+    custom_t *custom_ptr = MM_ALLOC(custom_t);
+
+    EXPECT_TRUE(MM_VERIFY_PTR(iptr, int));
+    EXPECT_TRUE(MM_VERIFY_PTR(cptr, char));
+    EXPECT_TRUE(MM_VERIFY_PTR(custom_ptr, custom_t));
+
+    EXPECT_FALSE(MM_VERIFY_PTR(iptr, char));
+    EXPECT_FALSE(MM_VERIFY_PTR(iptr, custom_t));
+
+    EXPECT_FALSE(MM_VERIFY_PTR(cptr, int));
+    EXPECT_FALSE(MM_VERIFY_PTR(cptr, custom_t));
+    
+    EXPECT_FALSE(MM_VERIFY_PTR(custom_ptr, int));
+    EXPECT_FALSE(MM_VERIFY_PTR(custom_ptr, char));
+
+    MM_DEALLOC(iptr);
+    MM_DEALLOC(cptr);
+    MM_DEALLOC(custom_ptr);
+}
+
+void error_callback([[maybe_unused]] void *addr, [[maybe_unused]] int code) {
+    fprintf(stderr, "error\n");
+    exit(code);
+}
+
+
+TEST(TestVerifyPtr, TestTypeDisRep)
+{
+    mm_init();
+    mm_attach_callback(error_callback);
+
+    int *iptr = MM_ALLOC(int);
+
+    EXPECT_EXIT(MM_VERIFY_PTR(iptr, char),
+                testing::ExitedWithCode(TYPE_DISREP), "error");
+   
+    MM_DEALLOC(iptr);
+}
+
+
